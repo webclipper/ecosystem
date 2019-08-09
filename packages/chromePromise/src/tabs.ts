1c91853176452cabfa6916bda8409e1fb6c80e5f
DiamondYuan <admin@diamondyuan.com>
2019-08-10 01:36:22 +0800

    feat(chrome promise): add executeScript for tabs

--- packages/chromePromise/src/tabs.ts
+++ packages/chromePromise/src/tabs.ts
@@ -5,16 +5,26 @@ class Tabs {
     });
   }
 
-  public sendMessage<T = any>(
-    tabId: number,
-    message: any,
-    options?: chrome.tabs.MessageSendOptions
-  ) {
+  public sendMessage<T = any>(tabId: number, message: any, options?: chrome.tabs.MessageSendOptions) {
     return new Promise<T>(resolve => {
       if (options) {
         chrome.tabs.sendMessage(tabId, message, options, resolve);
       } else {
         chrome.tabs.sendMessage(tabId, message, resolve);
+      }
+    });
+  }
+
+  public executeScript<T extends any[]>(details: chrome.tabs.InjectDetails, tabId?: number): Promise<T> {
+    return new Promise(resolve => {
+      if (typeof tabId === 'number') {
+        chrome.tabs.executeScript(tabId, details, r => {
+          resolve(r as T);
+        });
+      } else {
+        chrome.tabs.executeScript(details, r => {
+          resolve(r as T);
+        });
       }
     });
   }
