a4604f43dbdc044919c9d4eb43a54e69263e147a
DiamondYuan <admin@diamondyuan.com>
2019-08-10 11:27:50 +0800

    fix: fix type of storageArea

--- packages/chromePromise/src/storage/storageArea.ts
+++ packages/chromePromise/src/storage/storageArea.ts
@@ -8,34 +8,63 @@ export default class StorageArea {
   }
 
   public clear() {
-    return new Promise(resolve => {
-      this.storage.clear(resolve);
+    return new Promise((resolve, reject) => {
+      this.storage.clear(() => {
+        let err = chrome.runtime.lastError;
+        if (chrome.runtime.lastError) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     });
   }
 
   public set(item: Object) {
-    return new Promise(resolve => {
-      this.storage.set(item, resolve);
+    return new Promise<void>((resolve, reject) => {
+      this.storage.set(item, () => {
+        let err = chrome.runtime.lastError;
+        if (chrome.runtime.lastError) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
     });
   }
   public remove(keys: string | string[]) {
-    return new Promise(resolve => {
+    return new Promise<void>(resolve => {
       this.storage.remove(keys, resolve);
     });
   }
 
   public get(keys?: string | string[] | Object | null) {
-    return new Promise(resolve => {
+    return new Promise<{ [key: string]: any }>((resolve, reject) => {
       if (isUndefined(keys)) {
-        this.storage.get(resolve);
-      } else {
-        this.storage.get(keys, resolve);
+        this.storage.get(items => {
+          let err = chrome.runtime.lastError;
+          if (chrome.runtime.lastError) {
+            reject(err);
+          } else {
+            resolve(items);
+          }
+        });
+        return;
       }
+
+      this.storage.get(keys, items => {
+        let err = chrome.runtime.lastError;
+        if (chrome.runtime.lastError) {
+          reject(err);
+        } else {
+          resolve(items);
+        }
+      });
     });
   }
 
   public getBytesInUse(keys?: string | string[] | null) {
-    return new Promise(resolve => {
+    return new Promise<number>(resolve => {
       if (isUndefined(keys)) {
         this.storage.getBytesInUse(resolve);
       } else {
