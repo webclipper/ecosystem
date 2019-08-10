a897fdc6e22bf5ec19d37ae75df5d418b2774a9b
DiamondYuan <admin@diamondyuan.com>
2019-08-11 00:37:45 +0800

    feat(chromePromise): add set getall remove for cookies

--- packages/chromePromise/src/cookies.ts
+++ packages/chromePromise/src/cookies.ts
@@ -4,6 +4,28 @@ class Cookies {
       chrome.cookies.get(details, resolve);
     });
   }
+
+  public set(details: chrome.cookies.SetDetails): Promise<chrome.cookies.Cookie | null> {
+    return new Promise(resolve => {
+      chrome.cookies.set(details, resolve);
+    });
+  }
+
+  public getAll(details: chrome.cookies.GetAllDetails): Promise<chrome.cookies.Cookie[]> {
+    return new Promise(resolve => {
+      chrome.cookies.getAll(details, resolve);
+    });
+  }
+
+  public remove(details: chrome.cookies.Details): Promise<chrome.cookies.Details> {
+    return new Promise(resolve => {
+      chrome.cookies.remove(details, resolve);
+    });
+  }
+
+  get onChanged() {
+    return chrome.cookies.onChanged;
+  }
 }
 
 export default new Cookies();
